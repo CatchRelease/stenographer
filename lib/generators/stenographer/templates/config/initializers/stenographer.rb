9d8b2b1cc9298a2749c94b6801d4ee2aac16c4bb
Michael Carey <mikecx@gmail.com>
2018-05-07 08:21:56 -0400

    Make the generator have the default values set.

--- lib/generators/stenographer/templates/config/initializers/stenographer.rb
+++ lib/generators/stenographer/templates/config/initializers/stenographer.rb
@@ -2,16 +2,16 @@
 
 Stenographer.configure do |config|
   # The application name to use for the home link name if no icon is defined.
-  # config.app_name = 'Stenography'
+  config.app_name = 'Stenography'
 
   # The string location of the icon to use for the header, based off your app/assets/images directory.
   # i.e. config.app_icon = 'logo' would serve app/assets/images/logo.png
   #
   # The max height is set to 20px so horizontal logos work best, if that doesn't work use the app_name instead.
-  # config.app_icon = nil
+  config.app_icon = nil
 
   # This list of available options for change types.
-  # config.change_types = %w[new improved fixed]
+  config.change_types = %w[new improved fixed]
 
   # The viewer configuration specifies who is allowed to view changes. By default both viewing and managing are both set
   # to true.
@@ -22,7 +22,7 @@ Stenographer.configure do |config|
   # config.viewer = Proc.new { |session|
   #   User.select(:id, :viewer).find_by(id: session[:user_id]).try(:viewer?) if session.present?
   # }
-  # config.viewer = true
+  config.viewer = true
 
   # The viewer configuration specifies who is allowed to manage changes. By default both viewing and managing are both set
   # to true.
@@ -33,5 +33,5 @@ Stenographer.configure do |config|
   # config.manager = Proc.new { |session|
   #   User.select(:id, :manager).find_by(id: session[:user_id]).try(:manager?) if session.present?
   # }
-  # config.manager = true
+  config.manager = true
 end
