9d1606aba21207662d55b4037eddbbaac92d1b09
Michael Carey <mikecx@gmail.com>
2018-06-22 13:03:51 -0700

    Provide some output on invalid oAuth setup.

--- app/controllers/stenographer/authentications_controller.rb
+++ app/controllers/stenographer/authentications_controller.rb
@@ -10,14 +10,19 @@ module Stenographer
       auth_hash = request.env['omniauth.auth']
       @hash = auth_hash
 
-      Authentication.find_or_create_by(provider: auth_hash.provider) do |authentication|
-        authentication.uid = auth_hash.uid
-        authentication.credentials = auth_hash.credentials.present? ? auth_hash.credentials.to_json : nil
-        authentication.source = auth_hash.to_json
-      end
+      if auth_hash.try(:provider).present?
+        Authentication.find_or_create_by(provider: auth_hash.provider) do |authentication|
+          authentication.uid = auth_hash.uid
+          authentication.credentials = auth_hash.credentials.present? ? auth_hash.credentials.to_json : nil
+          authentication.source = auth_hash.to_json
+        end
 
-      flash[:notice] = 'Successfully created the Authentication'
-      redirect_to admin_authentications_path
+        flash[:notice] = 'Successfully created the Authentication'
+        redirect_to admin_authentications_path
+      else
+        flash[:alert] = 'Did not receive oAuth provider info'
+        redirect_back(fallback_location: root_path)
+      end
     end
 
     def failure; end
