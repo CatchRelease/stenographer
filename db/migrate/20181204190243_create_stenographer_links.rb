67e78966b1f39c89e438e2eeb2dd120ed55cbf79
Michael Carey <467469+mikecx@users.noreply.github.com>
2018-12-04 17:45:09 -0800

    Require the URL at the data base level.

--- db/migrate/20181204190243_create_stenographer_links.rb
+++ db/migrate/20181204190243_create_stenographer_links.rb
@@ -1,7 +1,7 @@
 class CreateStenographerLinks < ActiveRecord::Migration[5.2]
   def change
     create_table :stenographer_links do |t|
-      t.string :url
+      t.string :url, null: false
       t.string :description
 
       t.timestamps
